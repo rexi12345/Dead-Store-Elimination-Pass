c8afcf92bb6747573274a3458932429e61385859
Relja <89637614+rexi12345@users.noreply.github.com>
2024-07-02 22:59:08 +0200

    Update 1.c

--- 1.c
+++ 1.c
@@ -3,11 +3,13 @@
 int main() {
     int x = 12, y = 53; //x is dead y is dead
 
-    x = 204; //this x is not dead
+    x = 204; //this x is not dead bcs its used for z
     int z;
-    z = x;
-    z = 12;
-    x = 42; //this one is not dead bcs its used in pf
+    z = x; //is dead because of next line
+    
+    z = 12; //is dead because its not used anywhere
+    //live 
+    x = 42;
     y = 12;
 
     printf("%d\n", x + y);
